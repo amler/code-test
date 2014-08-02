ff64c28140eb6c2a63a02e26a4e86fbfe11bfc3e
Heather Amler <heatheramler@yahoo.com>
2014-08-02 02:19:25 -0400

    New tableMaker object created

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,48 +1,43 @@
 
 var url = 'http://community.ally.com/svc/api/';
-var dataArray = [];
+
+var tableMaker = {
+	init: function(array) {
+		this.dataArray = array;
+		this.sortApy();
+	},
+	dataArray: [],
+	sortApy: function() {
+		this.dataArray.sort(function(a, b){
+			if (a.apy < b.apy) {
+				return 1;
+			} else if (a.apy > b.apy) {
+				return -1;
+			}
+			return 0;
+		});
+		this.renderTableRow();
+	},
+	renderTableRow: function() {
+		this.dataArray.forEach(function(bank){
+			var totalString = '<tr><td class="bank">'+ bank.name + '</td>';
+			totalString += '<td class="ap">' + bank.apy + '%</td>';
+			totalString += '<td>' + bank.earnings + '</td></tr>';
+			$('.target').append(totalString);
+		});
+	}
+};
+
+
 $.ajax({
 	type: "GET",
 	url: url,
 	contentType: "application/json; charset=utf-8",
 	dataType: "jsonp",
 	success: function (data, textStatus, jqXHR) {
-		dataArray = data.records;
-		// console.log('ajax ', data.records);
-		sortApy(dataArray);
+		tableMaker.init(data.records);
 	},
 	error: function (errorMessage) {
 		console.log(errorMessage);
 	}
 });
-
-function renderInfo(record) {
-	record.forEach(function(record){
-		var totalString = '<tr><td class="bank">'+ record.name + '</td>';
-		totalString += '<td class="ap">' + record.apy + '%</td>';
-		totalString += '<td>' + record.earnings + '</td></tr>';
-		$('tbody').append(totalString);
-	});
-}
-
-function sortApy(array) {
-
-	dataArray.sort(function(a, b){
-		var apyA = a.apy,
-			apyB = b.apy;
-		if (apyA < apyB) {
-			return 1;
-		} else if (apyA > apyB) {
-			return -1;
-		}
-		return 0;
-	});
-	console.log('sort ', dataArray);
-	renderInfo(dataArray);
-
-}
-// this.sort(function(a, b) {
-        
-//     });
-// fruits.sort();
-
